e564b42ef7304f232ec74b47337de3511a6cbc90
ngesa marvin <marvinngesa@gmail.com>
2018-01-23 08:42:11 +0300

    setup servo connection - Live!!

--- app/src/main/java/com/droidmarvin/interfacingservomotorusingpwm/MainActivity.java
+++ app/src/main/java/com/droidmarvin/interfacingservomotorusingpwm/MainActivity.java
@@ -2,11 +2,37 @@ package com.droidmarvin.interfacingservomotorusingpwm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.pio.Pwm;
+
+import java.io.IOException;
+import java.util.Timer;
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final String PWM_SERVO_BUS = "PWM1";
+
+    int i;
+
+    private Pwm mServoPwm;
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    // access the servo
+    private void setupServo (){
+        try {
+            PeripheralManagerService service = new PeripheralManagerService();
+            mServoPwm = service.openPwm(PWM_SERVO_BUS);
+        }catch (IOException e){
+            Log.e(TAG, "error creating Servo", e);
+        }
     }
 }
